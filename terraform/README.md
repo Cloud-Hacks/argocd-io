3473ebe696610182e8b6d480401c1bf61bbaa208
stackgen-automation <automation@stackgen.com>
2025-01-23 19:11:05 +0000

    Automated commit

---
+++ terraform/README.md
@@ -0,0 +1,3 @@
+# README
+This is a readme file for IaC generated with StackGen.
+You can modify your appStack -> [here](http://cloud.stackgen.com/appstacks/ba7c869c-1a8a-4c4d-8979-89bbe72f5ecc)
---
+++ terraform/outputs.tf
@@ -0,0 +1,166 @@
+
+output "aws_iam_role_send_slack_alert_role_arn" {
+  value = module.stackgen_422f5f4a-6435-576e-ba43-8ed8da67c2da.arn
+  sensitive = false
+}
+
+output "aws_iam_role_send_slack_alert_role_name" {
+  value = module.stackgen_422f5f4a-6435-576e-ba43-8ed8da67c2da.name
+  sensitive = false
+}
+
+output "aws_iam_role_send_email_role_arn" {
+  value = module.stackgen_2a2183a7-1765-589b-8ffe-ca32b33f8aa2.arn
+  sensitive = false
+}
+
+output "aws_iam_role_send_email_role_name" {
+  value = module.stackgen_2a2183a7-1765-589b-8ffe-ca32b33f8aa2.name
+  sensitive = false
+}
+
+output "aws_iam_role_api_role_arn" {
+  value = module.stackgen_bd26a852-abd3-5bab-9143-c6789f51e169.arn
+  sensitive = false
+}
+
+output "aws_iam_role_api_role_name" {
+  value = module.stackgen_bd26a852-abd3-5bab-9143-c6789f51e169.name
+  sensitive = false
+}
+
+
+output "aws_sns_lambda_event_source_mapping_1_arn" {
+  value = module.stackgen_fd60d9fe-21ed-432d-81f4-cd9086387c11.arn
+  sensitive = false
+}
+
+
+output "aws_dynamodb_my_table_arn" {
+  value = module.stackgen_e7fea63f-a080-52bf-b1ca-0b5a20b331b9.arn
+  sensitive = false
+}
+
+
+output "aws_sqs_movqs_arn" {
+  value = module.stackgen_6d67a292-6845-50bd-b166-3ddf40c9b1d7.arn
+  sensitive = false
+}
+
+
+output "aws_lambda_send_email_function_function_arn" {
+  value = module.stackgen_fb1cbc74-f10a-57aa-8add-3c80cf395929.function_arn
+  sensitive = false
+}
+
+output "aws_lambda_send_email_function_function_name" {
+  value = module.stackgen_fb1cbc74-f10a-57aa-8add-3c80cf395929.function_name
+  sensitive = false
+}
+
+output "aws_lambda_send_email_function_function_url" {
+  value = module.stackgen_fb1cbc74-f10a-57aa-8add-3c80cf395929.function_url
+  sensitive = false
+}
+
+output "aws_lambda_send_email_function_invoke_arn" {
+  value = module.stackgen_fb1cbc74-f10a-57aa-8add-3c80cf395929.invoke_arn
+  sensitive = false
+}
+
+output "aws_lambda_send_email_function_arn" {
+  value = module.stackgen_fb1cbc74-f10a-57aa-8add-3c80cf395929.arn
+  sensitive = false
+}
+
+output "aws_lambda_send_slack_alert_function_function_arn" {
+  value = module.stackgen_4b4bc922-3a79-557a-873a-3205d1bebe39.function_arn
+  sensitive = false
+}
+
+output "aws_lambda_send_slack_alert_function_function_name" {
+  value = module.stackgen_4b4bc922-3a79-557a-873a-3205d1bebe39.function_name
+  sensitive = false
+}
+
+output "aws_lambda_send_slack_alert_function_function_url" {
+  value = module.stackgen_4b4bc922-3a79-557a-873a-3205d1bebe39.function_url
+  sensitive = false
+}
+
+output "aws_lambda_send_slack_alert_function_invoke_arn" {
+  value = module.stackgen_4b4bc922-3a79-557a-873a-3205d1bebe39.invoke_arn
+  sensitive = false
+}
+
+output "aws_lambda_send_slack_alert_function_arn" {
+  value = module.stackgen_4b4bc922-3a79-557a-873a-3205d1bebe39.arn
+  sensitive = false
+}
+
+output "aws_lambda_api_function_function_arn" {
+  value = module.stackgen_c8c8b81f-c343-5db6-99dd-0dbcebeaab6a.function_arn
+  sensitive = false
+}
+
+output "aws_lambda_api_function_function_name" {
+  value = module.stackgen_c8c8b81f-c343-5db6-99dd-0dbcebeaab6a.function_name
+  sensitive = false
+}
+
+output "aws_lambda_api_function_function_url" {
+  value = module.stackgen_c8c8b81f-c343-5db6-99dd-0dbcebeaab6a.function_url
+  sensitive = false
+}
+
+output "aws_lambda_api_function_invoke_arn" {
+  value = module.stackgen_c8c8b81f-c343-5db6-99dd-0dbcebeaab6a.invoke_arn
+  sensitive = false
+}
+
+output "aws_lambda_api_function_arn" {
+  value = module.stackgen_c8c8b81f-c343-5db6-99dd-0dbcebeaab6a.arn
+  sensitive = false
+}
+
+
+output "aws_cloudwatch_log_group_aws_lambda_send_email_function_arn" {
+  value = module.stackgen_a957e438-15ef-54a4-a2f1-b37edb476a87.arn
+  sensitive = false
+}
+
+output "aws_cloudwatch_log_group_aws_lambda_send_email_function_name" {
+  value = module.stackgen_a957e438-15ef-54a4-a2f1-b37edb476a87.name
+  sensitive = false
+}
+
+output "aws_cloudwatch_log_group_aws_lambda_send_slack_alert_function_arn" {
+  value = module.stackgen_c0c732c9-15af-57c2-9de7-fc067dd7a921.arn
+  sensitive = false
+}
+
+output "aws_cloudwatch_log_group_aws_lambda_send_slack_alert_function_name" {
+  value = module.stackgen_c0c732c9-15af-57c2-9de7-fc067dd7a921.name
+  sensitive = false
+}
+
+output "aws_cloudwatch_log_group_aws_lambda_api_function_arn" {
+  value = module.stackgen_c6250e95-971e-5d10-8274-abce91f8b908.arn
+  sensitive = false
+}
+
+output "aws_cloudwatch_log_group_aws_lambda_api_function_name" {
+  value = module.stackgen_c6250e95-971e-5d10-8274-abce91f8b908.name
+  sensitive = false
+}
+
+
+output "aws_sns_var_topic_name_arn" {
+  value = module.stackgen_35eb45e7-0d0c-5c6e-b451-79ea31bc461e.arn
+  sensitive = false
+}
+
+output "aws_sns_movns_arn" {
+  value = module.stackgen_22f36cfa-89df-551a-a7e9-efc9e051cd01.arn
+  sensitive = false
+}
