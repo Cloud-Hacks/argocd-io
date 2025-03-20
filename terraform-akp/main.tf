5ee9919b8d151aa688200c4f6d0dcc67f418d912
afzal442 <afzal442@gmail.com>
2025-03-20 16:27:26 +0530

    add terraform akp

---
+++ terraform-akp/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    akp = {
+      source = "akuity/akp"
+      version = "~> 0.8.1"
+    }
+  }
+}
+
+provider "akp" {
+  org_name = "afzal09"
+}
+
+resource "akp_instance" "argocd" {
+  name = "my9instance-test"
+  argocd = {
+    "spec" = {
+      "instance_spec" = {
+        declarative_management_enabled = true
+      }
+      version = "v2.14.2"
+    }
+  }
+
+  argocd_cm = {
+    "accounts.admin"                 = "apiKey,login"
+    "exec.enabled"                   = true
+    "ga.anonymizeusers"              = false
+    "helm.enabled"                   = true
+    "kustomize.enabled"              = true
+    "server.rbac.log.enforce.enable" = false
+    "statusbadge.enabled"            = false
+    "ui.bannerpermanent"             = false
+    "users.anonymous.enabled"        = false
+  }
+  argocd_secret = {
+    "admin.password" = "$2a$10$Yswzbr69F.SyV/E80r/bDOEH2Z6RQAaL4pf6NP0DJxemGFxnbOtg2" # cleartext is omgwtfbbq
+  }
+}
